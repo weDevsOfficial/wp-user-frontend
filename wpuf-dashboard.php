2b03e6d2611a08d8f8045cfa5f26e9d8cfefac32
Tareq Hasan <tareq1988@gmail.com>
2012-01-13 21:05:26 +0600

    nonce added

--- wpuf-dashboard.php
+++ wpuf-dashboard.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Handle's user dashboard functionality
  *
@@ -11,16 +12,16 @@
  */
 function wpuf_user_dashboard( $atts ) {
 
-    extract( shortcode_atts( array( 'post_type' => 'post' ), $atts ) );
-    
+    extract( shortcode_atts( array('post_type' => 'post'), $atts ) );
+
     if ( is_user_logged_in() ) {
         wpuf_user_dashboard_post_list( $post_type );
     } else {
-        printf( __("This page is restricted. Please %s to view this page.", 'wpuf' ), wp_loginout( '', false) );
+        printf( __( "This page is restricted. Please %s to view this page.", 'wpuf' ), wp_loginout( '', false ) );
     }
 }
 
-add_shortcode('wpuf_dashboard', 'wpuf_user_dashboard');
+add_shortcode( 'wpuf_dashboard', 'wpuf_user_dashboard' );
 
 /**
  * List's all the posts by the user
@@ -34,21 +35,21 @@ add_shortcode('wpuf_dashboard', 'wpuf_user_dashboard');
 function wpuf_user_dashboard_post_list( $post_type ) {
     global $wpdb, $userdata;
 
-    get_currentuserinfo(); // grabs the user info and puts into vars
-
+    $userdata = get_userdata( $userdata->ID );
     //delete post
-    if ( isset($_REQUEST['action']) && $_REQUEST['action'] == "del") {
+    if ( isset( $_REQUEST['action'] ) && $_REQUEST['action'] == "del" ) {
         $nonce = $_REQUEST['_wpnonce'];
-        if (! wp_verify_nonce($nonce, 'wpuf_del') ) die("Security check");
+        if ( !wp_verify_nonce( $nonce, 'wpuf_del' ) )
+            die( "Security check" );
 
         //check, if the requested user is the post author
-        $post = get_post($_REQUEST['pid']);
-        
-        if( $post->post_author == $userdata->ID) {
-            wp_delete_post($_REQUEST['pid']);
+        $post = get_post( $_REQUEST['pid'] );
+
+        if ( $post->post_author == $userdata->ID ) {
+            wp_delete_post( $_REQUEST['pid'] );
             echo '<div class="success">' . __( 'Post Deleted', 'wpuf' ) . '</div>';
         } else {
-            echo '<div class="error">'. __( 'You are not the post author. Cheeting huh!', 'wpuf' ) . '</div>';
+            echo '<div class="error">' . __( 'You are not the post author. Cheeting huh!', 'wpuf' ) . '</div>';
         }
     }
 
@@ -57,14 +58,14 @@ function wpuf_user_dashboard_post_list( $post_type ) {
             WHERE post_author = $userdata->ID AND post_type = '$post_type'
             AND (post_status = 'publish' OR post_status = 'pending' OR post_status = 'draft' OR post_status = 'future') ";
 
-    $total = $wpdb->get_var($sql);
+    $total = $wpdb->get_var( $sql );
 
     //setup the pagination variables
     $pagenum = isset( $_GET['pagenum'] ) ? absint( $_GET['pagenum'] ) : 1;
-    $limit = ( get_option( 'wpuf_list_post_range' ) ) ? get_option( 'wpuf_list_post_range' ): 10;
+    $limit = ( get_option( 'wpuf_list_post_range' ) ) ? get_option( 'wpuf_list_post_range' ) : 10;
     $offset = ( $pagenum - 1 ) * $limit;
     $num_of_pages = ( $total > 0 ) ? ceil( $total / $limit ) : 0;
-    
+
     $page_links = paginate_links( array(
         'base' => add_query_arg( 'pagenum', '%#%' ),
         'format' => '',
@@ -72,23 +73,23 @@ function wpuf_user_dashboard_post_list( $post_type ) {
         'next_text' => __( '&raquo;', 'aag' ),
         'total' => $num_of_pages,
         'current' => $pagenum
-    ) );
-    
+            ) );
+
     //get the posts
     $sql = "SELECT ID, post_title, post_name, post_status, post_date FROM $wpdb->posts
             WHERE post_author = $userdata->ID AND post_type = '$post_type'
             AND (post_status = 'publish' OR post_status = 'pending' OR post_status = 'draft' OR post_status = 'future') ORDER BY post_date DESC LIMIT $offset, $limit";
-    
-    $posts = $wpdb->get_results($sql);
+
+    $posts = $wpdb->get_results( $sql );
     ?>
 
     <h2 class="page-head">
-        <span class="colour"><?php printf(__("%s's Dashboard", 'wpuf'), $userdata->user_login); ?></span>
+        <span class="colour"><?php printf( __( "%s's Dashboard", 'wpuf' ), $userdata->user_login ); ?></span>
     </h2>
     <?php if ( $posts ) { ?>
 
-        <?php if( get_option( 'wpuf_list_post_count' ) == 'yes' ) { ?>
-        <div class="post_count"><?php _e( 'You have created', 'wpuf' ); ?> <?php echo "<span>$total</span> $post_type"; ?></div>
+        <?php if ( get_option( 'wpuf_list_post_count' ) == 'yes' ) { ?>
+            <div class="post_count"><?php _e( 'You have created', 'wpuf' ); ?> <?php echo "<span>$total</span> $post_type"; ?></div>
         <?php } ?>
 
         <table class="wpuf-table" cellpadding="0" cellspacing="0">
@@ -96,78 +97,84 @@ function wpuf_user_dashboard_post_list( $post_type ) {
                 <tr>
                     <th><?php _e( 'Title', 'wpuf' ); ?></th>
                     <th><?php _e( 'Status', 'wpuf' ); ?></th>
-                    <?php if ( get_option( 'wpuf_sub_charge_posting' ) == 'yes' ) echo '<th>Payment</th>'; ?>
+                    <?php if ( get_option( 'wpuf_sub_charge_posting' ) == 'yes' )
+                        echo '<th>Payment</th>'; ?>
                     <th><?php _e( 'Options', 'wpuf' ); ?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php wp_reset_query() ?>
-                <?php foreach($posts as $p) { ?>
-                <tr>
-                    <td>
-                        <?php if ($p->post_status == 'pending' || $p->post_status == 'draft' || $p->post_status == 'future') { ?>
+                <?php foreach ($posts as $p) { ?>
+                    <tr>
+                        <td>
+                            <?php if ( $p->post_status == 'pending' || $p->post_status == 'draft' || $p->post_status == 'future' ) { ?>
 
-                                <?php echo wptexturize($p->post_title); ?>
+                                <?php echo wptexturize( $p->post_title ); ?>
 
-                        <?php } else { ?>
+                            <?php } else { ?>
 
-                                <a href="<?php echo get_permalink($p->ID) ?>"><?php echo wptexturize( $p->post_title ); ?></a>
+                                <a href="<?php echo get_permalink( $p->ID ) ?>"><?php echo wptexturize( $p->post_title ); ?></a>
 
-                        <?php } ?>
-                    </td>
-                    <td>
-                        <?php wpuf_show_post_status($p->post_status) ?>
-                    </td>
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php wpuf_show_post_status( $p->post_status ) ?>
+                        </td>
 
-                    <?php if ( get_option( 'wpuf_sub_charge_posting' ) == 'yes' ) {
-                        $order_id = get_post_meta( $p->ID, 'wpuf_order_id', true);
-                        ?>
-                    <td>
-                        <?php if( $p->post_status == 'pending' && $order_id ) { ?>
-                            <a href="<?php echo get_permalink( get_option( 'wpuf_sub_pay_page' ) ); ?>?action=wpuf_pay&type=post&post_id=<?php echo $p->ID; ?>">Pay Now</a>
-                        <?php } ?>
-                    </td>
-                    <?php } ?>
-
-                    <td>
-                        <?php if(get_option('wpuf_can_edit_post') == 'yes') { ?>
-                            <?php $edit_page = get_option('wpuf_edit_page_url'); ?>
-                            <a href="<?php echo $edit_page ?>?pid=<?php echo $p->ID ?>">Edit</a>
-                        <?php } else { ?>
-                            &nbsp;
+                        <?php
+                        if ( get_option( 'wpuf_sub_charge_posting' ) == 'yes' ) {
+                            $order_id = get_post_meta( $p->ID, 'wpuf_order_id', true );
+                            ?>
+                            <td>
+                                <?php if ( $p->post_status == 'pending' && $order_id ) { ?>
+                                    <a href="<?php echo get_permalink( get_option( 'wpuf_sub_pay_page' ) ); ?>?action=wpuf_pay&type=post&post_id=<?php echo $p->ID; ?>">Pay Now</a>
+                                <?php } ?>
+                            </td>
                         <?php } ?>
 
-                        <?php if(get_option('wpuf_can_del_post') == 'yes') { ?>
-                            <a href="<?php echo wp_nonce_url("?action=del&pid=".$p->ID, 'wpuf_del') ?>" onclick="return confirm('Are you sure to delete this post?');"><span style="color: red;">Delete</span></a>
-                        <?php } ?>
-                    </td>
-                </tr>
+                        <td>
+                            <?php if ( get_option( 'wpuf_can_edit_post' ) == 'yes' ) { ?>
+                                <?php
+                                $edit_page = (int) get_option( 'wpuf_edit_page_url' );
+                                $url = get_permalink( $edit_page );
+                                ?>
+                                <a href="<?php echo wp_nonce_url( $url . '?pid=' . $p->ID, 'wpuf_edit' ); ?>">Edit</a>
+                            <?php } else { ?>
+                                &nbsp;
+                            <?php } ?>
+
+                            <?php if ( get_option( 'wpuf_can_del_post' ) == 'yes' ) { ?>
+                                <a href="<?php echo wp_nonce_url( "?action=del&pid=" . $p->ID, 'wpuf_del' ) ?>" onclick="return confirm('Are you sure to delete this post?');"><span style="color: red;">Delete</span></a>
+                            <?php } ?>
+                        </td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
 
         <div class="wpuf-pagination">
-            <?php if( $page_links ) echo $page_links; ?>
+            <?php if ( $page_links )
+                echo $page_links; ?>
         </div>
-        
+
     <?php } else { ?>
-        
+
         <h3><?php _e( 'Currently you have no post', 'wpuf' ); ?></h3>
-        
+
     <?php } ?>
 
 
-    <?php if( get_option( 'wpuf_list_user_info' ) == 'yes' ) { ?>
-    <div class="wpuf-author">
-        <h3><?php _e('Author Info', 'wpuf'); ?></h3>
-        <div class="wpuf-author-inside odd">
-            <div class="wpuf-user-image"><?php echo get_avatar( $userdata->user_email, 80 ); ?></div>
-            <div class="wpuf-author-body">
-                <p class="wpuf-user-name"><a href="<?php echo get_author_posts_url( $userdata->ID ); ?>"><?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->display_name ); ?></a></p>
-                <p class="wpuf-author-info"><?php echo $userdata->description; ?></p>
+    <?php if ( get_option( 'wpuf_list_user_info' ) == 'yes' ) { ?>
+        <div class="wpuf-author">
+            <h3><?php _e( 'Author Info', 'wpuf' ); ?></h3>
+            <div class="wpuf-author-inside odd">
+                <div class="wpuf-user-image"><?php echo get_avatar( $userdata->user_email, 80 ); ?></div>
+                <div class="wpuf-author-body">
+                    <p class="wpuf-user-name"><a href="<?php echo get_author_posts_url( $userdata->ID ); ?>"><?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->display_name ); ?></a></p>
+                    <p class="wpuf-author-info"><?php echo $userdata->description; ?></p>
+                </div>
             </div>
-        </div>
-    </div><!-- .author -->
+        </div><!-- .author -->
     <?php } ?>
 
     <?php

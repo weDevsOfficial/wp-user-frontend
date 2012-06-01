dd7a78009f003a9e5e214f21b09cf946fac1e52e
Tareq Hasan <tareq1988@gmail.com>
2012-06-01 09:20:57 +0600

    fixed custom field form, formatting was broken by previous release

--- admin/wpuf-admin-meta.php
+++ admin/wpuf-admin-meta.php
@@ -18,7 +18,7 @@ function wpuf_custom_fields_main() {
     global $custom_fields, $wpdb;
     ?>
 
-    <div class="wrap wpuf-admin">
+    <div class="wrap wpuf-admin custom-fields">
         <div class="icon32" id="icon-options-general"><br></div>
         <h2>Custom Field Options</h2>
 
@@ -64,7 +64,9 @@ function wpuf_custom_fields_main() {
         ?>
 
         <form action="" method="post" style="margin-top: 20px;">
+
             <?php wp_nonce_field( 'wpuf_add', 'wpuf_add' ); ?>
+
             <table class="widefat meta" style="width: 850px">
                 <thead>
                     <tr>
@@ -72,18 +74,81 @@ function wpuf_custom_fields_main() {
                     </tr>
                 </thead>
 
-                <?php wpuf_build_form( $custom_fields ); ?>
-
-                <tr valign="top" id="wpuf_field_values_row" style="display: none;">
-                    <td scope="row" class="label"><label for="wpuf_field_values">Values</label></td>
-                    <td>
-                        <textarea name="field_values" id="wpuf_field_values" cols="30"></textarea>
-                        <span class="description"><br>This will be used as option fields. Please separate values with comma</span>
-                    </td>
-                </tr>
+                <?php //wpuf_build_form( $custom_fields ); ?>
+                <?php _e( '', 'wpuf' ); ?>
+                <tbody>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="field"><?php _e( 'Field Name', 'wpuf' ) ?></label></td>
+                        <td>
+                            <input type="text" size="25" style="" id="field" value="" name="field" />
+                            <span class="description"><?php _e( 'Name without space. Will be used to store the value in this custom field', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="label"><?php _e( 'Label', 'wpuf' ); ?></label></td>
+                        <td>
+                            <input type="text" size="25" style="" id="label" value="" name="label" />
+                            <span class="description"><?php _e( 'This will be used as your input fields title', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="help"><?php _e( 'Help Text', 'wpuf' ); ?></label></td>
+                        <td>
+                            <input type="text" size="25" style="" id="help" value="" name="help" />
+                            <span class="description"><?php _e( 'Text will be shown to user as help text', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="required"><?php _e( 'Required', 'wpuf' ); ?></label></td>
+                        <td>
+                            <select id="required" name="required">
+                                <option value="no"><?php _e( 'No', 'wpuf' ); ?></option>
+                                <option value="yes"><?php _e( 'Yes', 'wpuf' ); ?></option>
+                            </select>
+                            <span class="description"><?php _e( 'A validation criteria. User must provide input in that field', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="region"><?php _e( 'Region', 'wpuf' ); ?></label></td>
+                        <td>
+                            <select id="region" name="region">
+                                <option value="top"><?php _e( 'Top', 'wpuf' ); ?></option>
+                                <option value="description"><?php _e( 'Before Description', 'wpuf' ); ?></option>
+                                <option value="tag"><?php _e( 'After Description', 'wpuf' ); ?></option>
+                                <option value="bottom"><?php _e( 'Bottom', 'wpuf' ); ?></option>
+                            </select>
+                            <span class="description"><?php _e( 'Where do you want to show this input field?', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="order"><?php _e( 'Order', 'wpuf' ); ?></label></td>
+                        <td>
+                            <input name="order" value="" id="order" style="" size="2" type="text">
+                            <span class="description"><?php _e( 'Which order this input field will show in a region', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <td scope="row" class="label"><label for="type"><?php _e( 'Type', 'wpuf' ); ?></label></td>
+                        <td>
+                            <select name="type" id="type" onchange="wpuf_show(this)">
+                                <option value="text"><?php _e( 'Text Box', 'wpuf' ); ?></option>
+                                <option value="textarea"><?php _e( 'Text Area', 'wpuf' ); ?></option>
+                                <option value="select"><?php _e( 'Dropdown', 'wpuf' ); ?></option>
+                            </select>
+                            <span class="description"></span>
+                        </td>
+                    </tr>
+                    <tr valign="top" id="wpuf_field_values_row" style="display: none;">
+                        <td scope="row" class="label"><label for="wpuf_field_values"><?php _e( 'Values', 'wpuf' ); ?></label></td>
+                        <td>
+                            <textarea name="field_values" id="wpuf_field_values" cols="30"></textarea>
+                            <span class="description"><br><?php _e( 'This will be used as option fields. Please separate values with comma', 'wpuf' ); ?></span>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
 
-            <input name="wpuf_add_custom" type="submit" class="button-primary" value="<?php _e( 'Add Field' ) ?>" style="margin-top: 10px;" />
+            <input name="wpuf_add_custom" type="submit" class="button-primary" value="<?php _e( 'Add Field', 'wpuf' ) ?>" style="margin-top: 10px;" />
 
         </form>
 
@@ -156,7 +221,7 @@ function wpuf_custom_fields_edit() {
     $id = intval( $_GET['id'] );
     ?>
 
-    <div class="wrap">
+    <div class="wrap wpuf-admin">
 
         <?php
         //update the fields
@@ -203,17 +268,8 @@ function wpuf_custom_fields_edit() {
             }
         } //finished updating
         //now show it
-        $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}wpuf_customfields WHERE `id`=$id", OBJECT );
-
-        $values = array(
-            "field" => "$row->field",
-            "label" => "$row->label",
-            "help" => "$row->desc",
-            "required" => "$row->required",
-            "region" => "$row->region",
-            "order" => "$row->order",
-            "type" => "$row->type",
-        );
+        $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}wpuf_customfields WHERE `id`=$id" );
+        //var_dump( $row );
         ?>
 
 
@@ -227,18 +283,80 @@ function wpuf_custom_fields_edit() {
                         </tr>
                     </thead>
 
-                    <?php wpuf_build_form( $custom_fields, $values, false ); ?>
-
-                    <tr valign="top" id="wpuf_field_values_row" style="display: none;">
-                        <td scope="row" class="label"><label for="wpuf_field_values">Values</label></td>
-                        <td>
-                            <textarea name="field_values" id="wpuf_field_values" cols="30"><?php echo $row->values; ?></textarea>
-                            <span class="description"><br>This will be used as option fields. Please separate values with comma</span>
-                        </td>
-                    </tr>
+                    <?php //wpuf_build_form( $custom_fields, $values, false ); ?>
+                    <tbody>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="field"><?php _e( 'Field Name', 'wpuf' ) ?></label></td>
+                            <td>
+                                <input type="text" size="25" style="" id="field" value="<?php echo esc_attr( $row->field ); ?>" name="field" />
+                                <span class="description"><?php _e( 'Name without space. Will be used to store the value in this custom field', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="label"><?php _e( 'Label', 'wpuf' ); ?></label></td>
+                            <td>
+                                <input type="text" size="25" style="" id="label" value="<?php echo esc_attr( $row->label ); ?>" name="label" />
+                                <span class="description"><?php _e( 'This will be used as your input fields title', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="help"><?php _e( 'Help Text', 'wpuf' ); ?></label></td>
+                            <td>
+                                <input type="text" size="25" style="" id="help" value="<?php echo esc_attr( $row->desc ); ?>" name="help" />
+                                <span class="description"><?php _e( 'Text will be shown to user as help text', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="required"><?php _e( 'Required', 'wpuf' ); ?></label></td>
+                            <td>
+                                <select id="required" name="required">
+                                    <option value="no"<?php selected( $row->required, 'no' ); ?>><?php _e( 'No', 'wpuf' ); ?></option>
+                                    <option value="yes"<?php selected( $row->required, 'yes' ); ?>><?php _e( 'Yes', 'wpuf' ); ?></option>
+                                </select>
+                                <span class="description"><?php _e( 'A validation criteria. User must provide input in that field', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="region"><?php _e( 'Region', 'wpuf' ); ?></label></td>
+                            <td>
+                                <select id="region" name="region">
+                                    <option value="top"<?php selected( $row->region, 'top' ); ?>><?php _e( 'Top', 'wpuf' ); ?></option>
+                                    <option value="description"<?php selected( $row->region, 'description' ); ?>><?php _e( 'Before Description', 'wpuf' ); ?></option>
+                                    <option value="tag"<?php selected( $row->region, 'tag' ); ?>><?php _e( 'After Description', 'wpuf' ); ?></option>
+                                    <option value="bottom"<?php selected( $row->region, 'bottom' ); ?>><?php _e( 'Bottom', 'wpuf' ); ?></option>
+                                </select>
+                                <span class="description"><?php _e( 'Where do you want to show this input field?', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="order"><?php _e( 'Order', 'wpuf' ); ?></label></td>
+                            <td>
+                                <input name="order" value="<?php echo esc_attr( $row->order ); ?>" id="order" style="" size="2" type="text">
+                                <span class="description"><?php _e( 'Which order this input field will show in a region', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                        <tr valign="top">
+                            <td scope="row" class="label"><label for="type"><?php _e( 'Type', 'wpuf' ); ?></label></td>
+                            <td>
+                                <select name="type" id="type" onchange="wpuf_show(this)">
+                                    <option value="text"<?php selected( $row->type, 'text' ); ?>><?php _e( 'Text Box', 'wpuf' ); ?></option>
+                                    <option value="textarea"<?php selected( $row->type, 'textarea' ); ?>><?php _e( 'Text Area', 'wpuf' ); ?></option>
+                                    <option value="select"<?php selected( $row->type, 'select' ); ?>><?php _e( 'Dropdown', 'wpuf' ); ?></option>
+                                </select>
+                                <span class="description"></span>
+                            </td>
+                        </tr>
+                        <tr valign="top" id="wpuf_field_values_row" style="display: none;">
+                            <td scope="row" class="label"><label for="wpuf_field_values"><?php _e( 'Values', 'wpuf' ); ?></label></td>
+                            <td>
+                                <textarea name="field_values" id="wpuf_field_values" cols="30"><?php echo esc_textarea( $row->values ); ?></textarea>
+                                <span class="description"><br><?php _e( 'This will be used as option fields. Please separate values with comma', 'wpuf' ); ?></span>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
 
-                <input name="wpuf_edit_custom" type="submit" class="button-primary" value="<?php _e( 'Update Field' ) ?>" style="margin-top: 10px;" />
+                <input name="wpuf_edit_custom" type="submit" class="button-primary" value="<?php _e( 'Update Field', 'wpuf' ) ?>" style="margin-top: 10px;" />
 
             </form>
         <?php } else { ?>

db1da000c336bc764eb0f808861bdd003e4dd56e
Tareq Hasan <tareq1988@gmail.com>
2012-06-03 19:49:03 +0600

    featured image upload script and init methods

--- js/wpuf.js
+++ js/wpuf.js
@@ -1,83 +1,162 @@
 jQuery(document).ready(function($) {
-    //process the contact us form
-    $('#wpuf_new_post_form, #wpuf_edit_post_form').submit(function() {
 
-        var form = $(this);
+    var WPUF_Obj = {
+        init: function () {
+            $('#wpuf_new_post_form, #wpuf_edit_post_form').on('submit', this.checkSubmit);
+            $('.wpuf-post-form').on('click', 'a.wpuf-del-ft-image', this.removeFeatImg);
 
-        form.find('.requiredField').each(function() {
-            if( $(this).hasClass('invalid') ) {
-                $(this).removeClass('invalid');
-            }
-        });
+            //editprofile password strength
+            $('#pass1').val('').keyup( this.passStrength );
+            $('#pass2').val('').keyup( this.passStrength );
+            $('#pass-strength-result').show();
 
-        var hasError = false;
+            //initialize the featured image uploader
+            this.featImgUploader();
+        },
+        checkSubmit: function () {
+            var form = $(this);
 
-        $(this).find('.requiredField').each(function() {
-            var el = $(this),
-                labelText = el.prev('label').text();
-
-            if(jQuery.trim(el.val()) == '') {
-                el.addClass('invalid');
-                hasError = true;
-            } else if(el.hasClass('email')) {
-                var emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-                if(!emailReg.test($.trim(el.val()))) {
-                    el.addClass('invalid');
-                    hasError = true;
+            form.find('.requiredField').each(function() {
+                if( $(this).hasClass('invalid') ) {
+                    $(this).removeClass('invalid');
                 }
-            } else if(el.hasClass('cat')) {
-                if( el.val() == '-1' ) {
-                    el.addClass('invalid');
-                    hasError = true;
-                }
-            }
-        });
-
-        if( ! hasError ) {
-            $(this).find('input[type=submit]').attr({
-                'value': wpuf.postingMsg,
-                'disabled': true
             });
 
-            return true;
+            var hasError = false;
+
+            $(this).find('.requiredField').each(function() {
+                var el = $(this),
+                labelText = el.prev('label').text();
+
+                if(jQuery.trim(el.val()) == '') {
+                    el.addClass('invalid');
+                    hasError = true;
+                } else if(el.hasClass('email')) {
+                    var emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+                    if(!emailReg.test($.trim(el.val()))) {
+                        el.addClass('invalid');
+                        hasError = true;
+                    }
+                } else if(el.hasClass('cat')) {
+                    if( el.val() == '-1' ) {
+                        el.addClass('invalid');
+                        hasError = true;
+                    }
+                }
+            });
+
+            if( ! hasError ) {
+                $(this).find('input[type=submit]').attr({
+                    'value': wpuf.postingMsg,
+                    'disabled': true
+                });
+
+                return true;
+            }
+
+            return false;
+        },
+        passStrength: function () {
+            var pass1 = $('#pass1').val(), user = $('#user_login1').val(), pass2 = $('#pass2').val(), strength;
+
+            $('#pass-strength-result').removeClass('short bad good strong');
+            if ( ! pass1 ) {
+                $('#pass-strength-result').html( pwsL10n.empty );
+                return;
+            }
+
+            strength = passwordStrength(pass1, user, pass2);
+
+            switch ( strength ) {
+                case 2:
+                    $('#pass-strength-result').addClass('bad').html( pwsL10n['bad'] );
+                    break;
+                case 3:
+                    $('#pass-strength-result').addClass('good').html( pwsL10n['good'] );
+                    break;
+                case 4:
+                    $('#pass-strength-result').addClass('strong').html( pwsL10n['strong'] );
+                    break;
+                case 5:
+                    $('#pass-strength-result').addClass('short').html( pwsL10n['mismatch'] );
+                    break;
+                default:
+                    $('#pass-strength-result').addClass('short').html( pwsL10n['short'] );
+            }
+        },
+        featImgUploader: function() {
+            var uploader = new plupload.Uploader(wpuf.plupload);
+
+            uploader.bind('Init', function(up, params) {
+                //$('#cpm-upload-filelist').html("<div>Current runtime: " + params.runtime + "</div>");
+                });
+
+            $('#wpuf-ft-upload-pickfiles').click(function(e) {
+                uploader.start();
+                e.preventDefault();
+            });
+
+            uploader.init();
+
+            uploader.bind('FilesAdded', function(up, files) {
+                $.each(files, function(i, file) {
+                    $('#wpuf-ft-upload-filelist').append(
+                        '<div id="' + file.id + '">' +
+                        file.name + ' (' + plupload.formatSize(file.size) + ') <b></b>' +
+                        '</div>');
+                });
+
+                up.refresh(); // Reposition Flash/Silverlight
+                uploader.start();
+            });
+
+            uploader.bind('UploadProgress', function(up, file) {
+                $('#' + file.id + " b").html(file.percent + "%");
+            });
+
+            uploader.bind('Error', function(up, err) {
+                $('#wpuf-ft-upload-filelist').append("<div>Error: " + err.code +
+                    ", Message: " + err.message +
+                    (err.file ? ", File: " + err.file.name : "") +
+                    "</div>"
+                    );
+
+                up.refresh(); // Reposition Flash/Silverlight
+            });
+
+            uploader.bind('FileUploaded', function(up, file, response) {
+                var resp = $.parseJSON(response.response);
+                //$('#' + file.id + " b").html("100%");
+                $('#' + file.id).remove();
+                //console.log(resp);
+                if( resp.success ) {
+                    $('#wpuf-ft-upload-filelist').append(resp.html);
+                    $('#wpuf-ft-upload-pickfiles').hide();
+                }
+            });
+        },
+        removeFeatImg: function(e) {
+            e.preventDefault();
+
+            if(confirm(wpuf.confirmMsg)) {
+                var el = $(this),
+                    data = {
+                        'attach_id' : el.data('id'),
+                        'nonce' : wpuf.nonce,
+                        'action' : 'wpuf_feat_img_del'
+                    }
+
+                $.post(wpuf.ajaxurl, data, function(){
+                    $('#wpuf-ft-upload-pickfiles').show();
+                    el.parent().remove();
+                });
+            }
         }
+    };
 
-        return false;
-    });
+    //run the bootstrap
+    WPUF_Obj.init();
 
-    function check_pass_strength() {
-        var pass1 = $('#pass1').val(), user = $('#user_login1').val(), pass2 = $('#pass2').val(), strength;
-
-        $('#pass-strength-result').removeClass('short bad good strong');
-        if ( ! pass1 ) {
-            $('#pass-strength-result').html( pwsL10n.empty );
-            return;
-        }
-
-        strength = passwordStrength(pass1, user, pass2);
-
-        switch ( strength ) {
-            case 2:
-                $('#pass-strength-result').addClass('bad').html( pwsL10n['bad'] );
-                break;
-            case 3:
-                $('#pass-strength-result').addClass('good').html( pwsL10n['good'] );
-                break;
-            case 4:
-                $('#pass-strength-result').addClass('strong').html( pwsL10n['strong'] );
-                break;
-            case 5:
-                $('#pass-strength-result').addClass('short').html( pwsL10n['mismatch'] );
-                break;
-            default:
-                $('#pass-strength-result').addClass('short').html( pwsL10n['short'] );
-        }
-    }
-
-    //editprofile password strength
-    $('#pass1').val('').keyup( check_pass_strength );
-    $('#pass2').val('').keyup( check_pass_strength );
-    $('#pass-strength-result').show();
 });
 
 

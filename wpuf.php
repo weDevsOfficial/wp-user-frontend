9283984f3406069f148e3e901d96e75d292777bf
Tareq Hasan <tareq1988@gmail.com>
2012-06-15 15:54:50 +0600

    settings-options.php included

--- wpuf.php
+++ wpuf.php
@@ -14,6 +14,7 @@ if ( !class_exists( 'WeDevs_Settings_API' ) ) {
 }
 
 require_once 'wpuf-functions.php';
+require_once 'admin/settings-options.php';
 require_once 'admin/wpuf-options-value.php';
 
 if ( is_admin() ) {

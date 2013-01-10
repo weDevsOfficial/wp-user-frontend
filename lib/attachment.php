18a309add2e117bc3fa88771f8398ffafa9d00ee
samholford <samholford@gmail.com>
2013-01-10 16:58:17 +1300

    Add attachment help text

--- lib/attachment.php
+++ lib/attachment.php
@@ -68,6 +68,7 @@ class WPUF_Attachment {
         <li>
             <label><?php echo wpuf_get_option( 'attachment_label' ) ?></label>
             <div class="clear"></div>
+            <p class="description"><?php echo wpuf_get_option( 'attachment_help' ) ?></p>
         </li>
         <li>
             <div id="wpuf-attachment-upload-container">

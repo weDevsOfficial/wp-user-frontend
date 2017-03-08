20bf42169d658bc5c386d1098dc5daef6f569fa0
mahbubme <mail@mahbub.me>
2017-03-08 16:24:02 +0600

    fixed help text visible issue for radio & checkbox field

--- admin/form-builder/assets/js/components/form-radio_field/template.php
+++ admin/form-builder/assets/js/components/form-radio_field/template.php
@@ -8,6 +8,7 @@
                     :checked="is_selected(label)"
                     :class="class_names('radio_btns')"
                 > {{ label }}
+                <span v-if="field.help" class="wpuf-help">{{ field.help }}</span>
             </label>
         </li>
     </ul>

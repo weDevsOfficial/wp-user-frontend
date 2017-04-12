80e51ec3e5864e1db9cbd0054e42bf521aaabd42
Tareq Hasan <tareq1988@gmail.com>
2017-04-12 09:08:44 +0600

    Added function to get client IP

--- wpuf-functions.php
+++ wpuf-functions.php
@@ -1859,3 +1859,32 @@ function wpuf_create_sample_form( $post_title = 'Sample Form', $post_type = 'wpu
 
     return $form_id;
 }
+
+/**
+ * Get the client IP address
+ *
+ * @since 2.5.2
+ *
+ * @return string
+ */
+function wpuf_get_client_ip() {
+    $ipaddress = '';
+
+    if ( isset($_SERVER['HTTP_CLIENT_IP'] ) ) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if ( isset( $_SERVER['HTTP_X_FORWARDED'] ) ) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if ( isset( $_SERVER['HTTP_FORWARDED_FOR'] ) ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if ( isset( $_SERVER['HTTP_FORWARDED'] ) ) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}

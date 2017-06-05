c9cb8e7a8185e22a0abc6c117de27fb15326092b
Sk Shaikat <sk.shaikat18@gmail.com>
2017-06-05 13:03:36 +0600

    fix #136 When site language changed, Add Form/ Edit Form page display nothing.

--- admin/form-builder/assets/js/components/merge-tags/template.php
+++ admin/form-builder/assets/js/components/merge-tags/template.php
@@ -18,24 +18,27 @@
         </div><!-- .merge-tag-section -->
 
         <?php
-        $merge_tags = wpuf_cf_get_merge_tags();
+        if ( function_exists( 'wpuf_cf_get_merge_tags' ) ) {
 
-        foreach ($merge_tags as $section_key => $section) {
-            ?>
+            $merge_tags = wpuf_cf_get_merge_tags();
 
-            <div class="merge-tag-section">
-                <div class="merge-tag-head"><?php echo $section['title'] ?></div>
+            foreach ($merge_tags as $section_key => $section) {
+                ?>
 
-                <ul>
-                    <?php foreach ($section['tags'] as $key => $value) { ?>
-                        <li>
-                            <a href="#" v-on:click.prevent="insertField('<?php echo $key; ?>');"><?php echo $value; ?></a>
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div><!-- .merge-tag-section -->
+                <div class="merge-tag-section">
+                    <div class="merge-tag-head"><?php echo $section['title'] ?></div>
 
-            <?php
+                    <ul>
+                        <?php foreach ($section['tags'] as $key => $value) { ?>
+                            <li>
+                                <a href="#" v-on:click.prevent="insertField('<?php echo $key; ?>');"><?php echo $value; ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div><!-- .merge-tag-section -->
+
+                <?php
+            }
         }
         ?>
     </div><!-- .merge-tags -->

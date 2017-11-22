248f27db836b486646b30ec7d0a05184d59ac402
mahbubme <mail@mahbub.me>
2017-11-22 08:49:55 +0600

    Integrated WPUF with Dokan #178

---
+++ templates/dokan/post-listing.php
@@ -0,0 +1,7 @@
+<header class="dokan-dashboard-header">
+    <span class="pull-right">
+        <a href="<?php echo dokan_get_navigation_url( 'posts' ); ?>?action=new-post" class="dokan-btn dokan-btn-theme"><?php _e( '+ Add Post', 'wpuf' ); ?></a>
+    </span>
+</header><!-- .dokan-dashboard-header -->
+
+<?php echo do_shortcode( '[wpuf_dashboard]' ); ?>

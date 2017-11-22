1c6b8003a8dd51b77f13fe6ae43b0ae3a4ac0150
Tareq Hasan <tareq1988@gmail.com>
2017-11-22 11:14:09 +0600

    Updated settings page style

--- admin/settings-options.php
+++ admin/settings-options.php
@@ -371,7 +371,7 @@ function wpuf_settings_fields() {
             array(
                 'name'       => 'guest_email_body',
                 'label'    => __( 'Guest Email Body', 'wpuf' ),
-                'desc'     => __( "This sets the body of the emails sent to guest users. Please DON'T edit the '{activation_link}' part", 'wpuf' ),
+                'desc'     => __( "This sets the body of the emails sent to guest users. Please DON'T edit the <code>{activation_link}</code> part", 'wpuf' ),
                 'default'  => "Hey There, \r\n\r\nWe just received your guest post and now we want you to confirm your email so that we can verify the content and move on to the publishing process.\r\n\r\nPlease click the link below to verify: \r\n\r\n{activation_link}\r\n\r\nRegards,\r\n%sitename%",
                 'type'     => 'textarea',
             )

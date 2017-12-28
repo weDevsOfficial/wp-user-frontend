76eb27c76bddfac4c561744c42edaf404340918c
Towhidul Islam <itowhid06@users.noreply.github.com>
2017-12-28 14:32:18 +0600

    Bugfix/fixes 2.8.0 (#285)
    * subscription notification draft
    
    * Add sub notification event clear and update js

--- assets/js/wpuf-admin.js
+++ assets/js/wpuf-admin.js
@@ -1,26 +1,34 @@
 jQuery(function($) {
-    $('.pre-sub-exp-notify-date').hide();
-    $('.post-sub-exp-notify-date').hide();
-    $('.pre-sub-exp-sub').hide();
-    $('.pre-sub-exp-body').hide();
-    $('.post-sub-exp-sub').hide();
-    $('.post-sub-exp-body').hide();
+
+    function show_sub_sections() {
+        $('.pre-sub-exp-notify-date').show();
+        $('.post-sub-exp-notify-date').show();
+        $('.pre-sub-exp-sub').show();
+        $('.pre-sub-exp-body').show();
+        $('.post-sub-exp-sub').show();
+        $('.post-sub-exp-body').show()
+    }
+
+    function hide_sub_sections() {
+        $('.pre-sub-exp-notify-date').hide();
+        $('.post-sub-exp-notify-date').hide();
+        $('.pre-sub-exp-sub').hide();
+        $('.pre-sub-exp-body').hide();
+        $('.post-sub-exp-sub').hide();
+        $('.post-sub-exp-body').hide();
+    }
+
+    if ( $("#wpuf-wpuf_mails\\[enable_subs_notification\\]").attr('checked')) {
+        show_sub_sections();
+    } else {
+        hide_sub_sections();
+    }
 
     $("#wpuf-wpuf_mails\\[enable_subs_notification\\]").click( function() {
         if( $(this).prop("checked") ) {
-            $('.pre-sub-exp-notify-date').show();
-            $('.post-sub-exp-notify-date').show();
-            $('.pre-sub-exp-sub').show();
-            $('.pre-sub-exp-body').show();
-            $('.post-sub-exp-sub').show();
-            $('.post-sub-exp-body').show();
+            show_sub_sections();
         } else {
-            $('.pre-sub-exp-notify-date').hide();
-            $('.post-sub-exp-notify-date').hide();
-            $('.pre-sub-exp-sub').hide();
-            $('.pre-sub-exp-body').hide();
-            $('.post-sub-exp-sub').hide();
-            $('.post-sub-exp-body').hide();
+            hide_sub_sections();
         }
     });
 });

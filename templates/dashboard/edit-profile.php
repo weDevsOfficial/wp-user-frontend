28425df4242691351a296f975fce2f4a0f7c8c31
Towhidul Islam <itowhid06@gmail.com>
2017-12-28 13:00:37 +0600

    Fix edit profile page was not working

--- templates/dashboard/edit-profile.php
+++ templates/dashboard/edit-profile.php
@@ -1,6 +1,6 @@
 <?php global $current_user; ?>
 
-<form class="wpuf-form wpuf-form-add wpuf-update-profile-form" action="" method="post">
+<form class="wpuf-form wpuf-update-profile-form" action="" method="post">
 
     <div style="display: none;" class="wpuf-success"><?php _e( 'Profile updated successfully!', 'wpuf' ); ?></div>
     <div style="display: none;" class="wpuf-error"><?php _e( 'Something went wrong!', 'wpuf' ); ?></div>
@@ -31,17 +31,26 @@
                 <input type="email" class="input-text" name="email" id="email" value="<?php echo $current_user->user_email; ?>" required>
             </div>
         </li>
+        <div class="clear"></div>
 
         <li class="wpuf-el">
             <div class="wpuf-label" >
                 <label for="current_password"><?php _e( 'Current Password', 'wpuf' ); ?></label>
             </div>
             <div class="wpuf-fields" >
+                <input type="password" class="input-text" name="current_password" id="current_password" size="16" value="" autocomplete="off" />
+            </div>
+            <span class="wpuf-help"><?php _e( 'Leave this field empty to keep your password unchanged.', 'wpuf' ); ?></span>
+        </li>
+        <div class="clear"></div>
+
+        <li class="wpuf-el">
+            <div class="wpuf-label" >
+                <label for="pass1"><?php _e( 'New Password', 'wpuf' ); ?></label>
+            </div>
+            <div class="wpuf-fields" >
                 <input type="password" class="input-text" name="pass1" id="pass1" size="16" value="" autocomplete="off" />
             </div>
-            <span class="wpuf-help"><?php _e( 'If you  want to unchanged the password then just leave this section.', 'wpuf' ); ?></span>
-        </li>
-
             <span style="width: 100%;" id="pass-strength-result"><?php _e( 'Strength indicator', 'wpuf' ); ?></span>
             <script src="<?php echo site_url(); ?>/wp-includes/js/zxcvbn.min.js"></script>
             <script src="<?php echo admin_url(); ?>/js/password-strength-meter.js"></script>
@@ -58,6 +67,9 @@
                     convertEntities(pwsL10n);
                 } catch (e) {};
             </script>
+        </li>
+        <div class="clear"></div>
+
         <li class="wpuf-el">
             <div class="wpuf-label" >
                 <label for="pass2"><?php _e( 'Confirm New Password', 'wpuf' ); ?></label>
@@ -71,7 +83,7 @@
         <li class="wpuf-submit">
             <?php wp_nonce_field( 'wpuf-account-update-profile' ); ?>
             <input type="hidden" name="action" value="wpuf_account_update_profile">
-            <input type="submit" name="update_profile"  id="wpuf-account-update-profile" value="Update Profile">
+            <button type="submit" name="update_profile" id="wpuf-account-update-profile"><?php _e( 'Update Profile', 'wpuf' ); ?></button>
         </li>
     </ul>
 

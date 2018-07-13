105a24e3c9b04db5a92bb7d7805b1528594b87e9
mahbubme <mail@mahbub.me>
2018-07-13 15:25:07 +0600

    Fix array rendering problem

--- wpuf-functions.php
+++ wpuf-functions.php
@@ -838,11 +838,10 @@ function wpuf_show_custom_fields( $content ) {
                         if ( $modified_value ) {
                            $html .= sprintf( '<li><label>%s</label>: %s</li>', $attr['label'], make_clickable( $modified_value ) );
                         }
-                    } elseif ( ( $attr['input_type'] == 'checkbox' || $attr['input_type'] == 'multiselect' ) && is_array( $value ) ) {
+                    } elseif ( ( $attr['input_type'] == 'checkbox' || $attr['input_type'] == 'multiselect' ) && is_array( $value[0] ) ) {
 
                         if ( !empty( $value[0] ) ) {
-                            $glue = array( $separator, '', );
-                            $modified_value = implode( $glue, $value[0] );
+                            $modified_value = implode( $separator, $value[0] );
 
                             if ( $modified_value ) {
                                $html .= sprintf( '<li><label>%s</label>: %s</li>', $attr['label'], make_clickable( $modified_value ) );

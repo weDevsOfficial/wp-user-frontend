4870ad4474e1e3c4ce0949000f1e714aa0454b59
Raul Illana <raul.fernandez.illana@gmail.com>
2020-07-08 06:37:13 +0200

    Add multiple comma separated emails in email field
    Had to remove inherit jQuery validations for whole WPUF forms, caring has its own ones.

--- assets/js/frontend-form.js
+++ assets/js/frontend-form.js
@@ -26,6 +26,10 @@
             $('.wpuf-form').on('click', 'button#wpuf-account-update-profile', this.account_update_profile);
 
             $('.wpuf-form-add').on('submit', this.formSubmit);
+            
+            // remove jQuery inherit validation
+            $('.wpuf-form input[type="submit"]').attr( 'formnovalidate', 'formnovalidate' );
+            
             $('form#post').on('submit', this.adminPostSubmit);
             // $( '.wpuf-form').on('keyup', '#pass1', this.check_pass_strength );
 
@@ -717,10 +721,10 @@
 
         isValidEmail: function( email ) {
             var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            var res = false;
+            var res = true;
 
-            $(email.replace(/\,+$/, '').split(',')).each(function(i, elm) {
-                res = pattern.test(elm.trim()) ? true : false;
+            $(email.split(',')).each(function(i, elm) {
+                res = res && pattern.test(elm.trim());
             });
             
             return res;

f3fcf27f3e9417c07e8b131913182d34f220b888
Edi Amin <to.ediamin@gmail.com>
2020-08-07 15:19:44 +0600

    Minor formatting for pr/897

--- includes/class-frontend-form-post.php
+++ includes/class-frontend-form-post.php
@@ -191,31 +191,29 @@ class WPUF_Frontend_Form extends WPUF_Frontend_Render_Form {
         ];
 
         if ( ! empty( $this->form_fields ) ) {
-
             foreach ( $this->form_fields as $field ) {
-
                 if ( $field['template'] == 'taxonomy' ) {
                     $category_name = $field['name'];
 
-                    if ( isset( $_POST[$category_name] ) && is_array( $_POST[$category_name] ) ) { // WPCS: sanitization ok.
-                        $category = isset( $_POST[$category_name] ) ? array_map( 'sanitize_text_field', wp_unslash( $_POST[$category_name] ) ) : [];
-                        } else {
-                            $category = isset( $_POST[$category_name] ) ? sanitize_text_field( wp_unslash( $_POST[$category_name] ) ) : '';
-                        }
+                    if ( isset( $_POST[ $category_name ] ) && is_array( $_POST[ $category_name ] ) ) { // WPCS: sanitization ok.
+                        $category = isset( $_POST[ $category_name ] ) ? array_map( 'sanitize_text_field', wp_unslash( $_POST[ $category_name ] ) ) : [];
+                    } else {
+                        $category = isset( $_POST[ $category_name ] ) ? sanitize_text_field( wp_unslash( $_POST[ $category_name ] ) ) : '';
+                    }
 
-                        if ( $category != '' && $category != '0' && $category[0] != '-1' )  {
-                            if ( !is_array( $category ) && is_string( $category ) ) {
-                                $category_strings = explode( ',', $category );
-                                $cat_ids          = [];
+                    if ( $category != '' && $category != '0' && $category[0] != '-1' )  {
+                        if ( ! is_array( $category ) && is_string( $category ) ) {
+                            $category_strings = explode( ',', $category );
+                            $cat_ids          = [];
 
-                                foreach ( $category_strings as $key => $each_cat_string ) {
-                                    $cat_ids[]                = get_cat_ID( trim( $each_cat_string ) );
-                                    $postarr['post_category'] = $cat_ids;
-                                }
-                            } else {
-                                $postarr['post_category'] =$category;
+                            foreach ( $category_strings as $key => $each_cat_string ) {
+                                $cat_ids[]                = get_cat_ID( trim( $each_cat_string ) );
+                                $postarr['post_category'] = $cat_ids;
                             }
+                        } else {
+                            $postarr['post_category'] = $category;
                         }
+                    }
                 }
             }
         }

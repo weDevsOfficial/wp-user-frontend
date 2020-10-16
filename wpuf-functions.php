b99833275fe9e853acde5ef72a977485e159c5bc
Jason Alberino <jason@thedreadmachine.com>
2020-10-15 21:56:47 -0400

    Update wpuf-functions.php

--- wpuf-functions.php
+++ wpuf-functions.php
@@ -38,6 +38,9 @@ function wpuf_show_post_status( $status ) {
     } elseif ( $status == 'private' ) {
         $title     = __( 'Private', 'wp-user-frontend' );
         $fontcolor = '#bbbbbb';
+    } else {
+        $title     = __( $status, 'wp-user-frontend' );
+        $fontcolor = '#bbbbbb';    
     }
 
     $show_status = '<span style="color:' . $fontcolor . ';">' . $title . '</span>';
@@ -66,6 +69,9 @@ function wpuf_admin_post_status( $status ) {
     } elseif ( $status == 'future' ) {
         $title     = __( 'Scheduled', 'wp-user-frontend' );
         $fontcolor = '#bbbbbb';
+    } else {
+        $title     = __( $status, 'wp-user-frontend' );
+        $fontcolor = '#bbbbbb';    
     }
 
     echo wp_kses_post( '<span style="color:' . $fontcolor . ';">' . $title . '</span>' );

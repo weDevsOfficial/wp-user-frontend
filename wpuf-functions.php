ec88b1b8e5144b3939d4364b776889333d760562
zamandevs <32278210+zamandevs@users.noreply.github.com>
2021-02-22 16:34:18 +0600

    fix: address field not working when used with conditional logic (#1053)
    * fix: address field not working when used with conditional logic
    
    * li bring inside conditional
    
    * indentation fixed

--- wpuf-functions.php
+++ wpuf-functions.php
@@ -1069,13 +1069,15 @@ function wpuf_show_custom_fields( $content ) {
                                 }
                             }
 
-                            $address_html .= '<li>';
+                            if ( ! empty( $value ) ) {
+                                $address_html .= '<li>';
 
-                            if ( 'no' === $hide_label ) {
-                                $address_html .= '<label>' . $attr['address'][ $field_key ]['label'] . ': </label> ';
+                                if ( 'no' === $hide_label ) {
+                                    $address_html .= '<label>' . $attr['address'][ $field_key ]['label'] . ': </label> ';
+                                }
+
+                                $address_html .= ' ' . $value . '</li>';
                             }
-
-                            $address_html .= ' ' . $value . '</li>';
                         }
                     }
 

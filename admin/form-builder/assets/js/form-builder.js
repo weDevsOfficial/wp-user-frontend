f0d0f4e528bcd7fe1e7e67214bc421c349c6f168
zamandevs <zaman.devs@gmail.com>
2021-08-16 16:15:07 +0600

    assets link broken fixed #1152

--- admin/form-builder/assets/js/form-builder.js
+++ admin/form-builder/assets/js/form-builder.js
@@ -151,8 +151,8 @@
                     var el = $('#form-preview-stage .wpuf-form .field-items').eq(payload.toIndex);
                     if ('yes' == payload.field.is_meta && state.show_custom_field_tooltip) {
 
-                        var image_one  = wpuf_assets_url + '/images/custom-fields/settings.png';
-                        var image_two  = wpuf_assets_url + '/images/custom-fields/advance.png';
+                        var image_one  = wpuf_assets_url.url + '/images/custom-fields/settings.png';
+                        var image_two  = wpuf_assets_url.url + '/images/custom-fields/advance.png';
                         var html       = '<div class="wpuf-custom-field-instruction">';
                             html      += '<div class="step-one">';
                             html      += sprintf( '<p style="font-weight: 400">%s<strong><code>%s</code></strong>%s"</p>', __( 'Navigate through', 'wp-user-frontend' ), __( 'WP-admin > WPUF > Settings > Frontend Posting', 'wp-user-frontend' ), __( '- there you have to check the checkbox: "Show custom field data in the post content area', 'wp-user-frontend' ) );
@@ -177,7 +177,7 @@
                             if (result) {
                                 state.show_custom_field_tooltip = false;
                             } else {
-                                
+
                             }
                         } );
                     }

cead30b94fb727483f86933ead97bfbe3ad61f39
zamandevs <zaman.devs@gmail.com>
2021-09-23 14:16:09 +0600

    search in settings page implemented as instructed

--- admin/class-admin-settings.php
+++ admin/class-admin-settings.php
@@ -164,9 +164,10 @@ class WPUF_Admin_Settings {
                 ?>
             </div>
             <script>
+                (function () {
                 document.addEventListener('DOMContentLoaded',function () {
                     var tabs    = document.querySelector('.wpuf-settings-wrap').querySelectorAll('h2 a');
-                    var content = document.querySelector('.wpuf-settings-wrap .metabox-holder').querySelectorAll(':scope > div');
+                    var content = document.querySelectorAll('.wpuf-settings-wrap .metabox-holder th');
                     var close   = document.querySelector('#wpuf-search-section span');
 
                     var search_input = document.querySelector('#wpuf-settings-search');
@@ -175,27 +176,33 @@ class WPUF_Admin_Settings {
                         var search_value = e.target.value;
                         var value_tab  = [];
 
-                        if (search_value.length) {
+                        if ( search_value.length ) {
                             close.style.display = 'flex'
                             content.forEach(function (row, index) {
-                                var content_id = row.getAttribute('id');
-                                var tab_id     = content_id + '-tab';
 
-                                if (row.innerText.toLowerCase().includes( search_value ) === true) {
+                                var content_id = row.closest('div').getAttribute('id');
+                                var tab_id     = content_id + '-tab';
+                                var found_value = row.innerText.toLowerCase().includes( search_value );
+
+                                if ( found_value ){
+                                    row.closest('tr').style.display = 'table-row';
+                                }else {
+                                    row.closest('tr').style.display = 'none';
+                                }
+
+                                if ( 'wpuf_mails' === content_id ){
+                                    row.closest('tbody').querySelectorAll('tr').forEach(function (tr) {
+                                        tr.style.display = '';
+                                    });
+                                }
+
+                                if ( found_value === true && ! value_tab.includes( tab_id ) ) {
                                     value_tab.push(tab_id);
                                 }
                             })
 
-                            if ( value_tab.length ){
+                            if ( value_tab.length ) {
                                 document.getElementById(value_tab[0]).click();
-                                var rows = document.getElementById(value_tab[0].replace('-tab','')).querySelectorAll('tr')
-                                rows.forEach(function (row) {
-                                       if ( row.querySelector('label').innerText.toLowerCase().includes( search_value ) !== true ){
-                                           row.style.display = 'none';
-                                       }else{
-                                           row.style.display = 'block';
-                                       }
-                                })
                             }
 
                             tabs.forEach(function (tab) {
@@ -203,7 +210,7 @@ class WPUF_Admin_Settings {
                                 if ( ! value_tab.includes( tab_id ) ){
                                     document.getElementById(tab_id).style.display = 'none';
                                 }else {
-                                    document.getElementById(tab_id).style.display = '';
+                                    document.getElementById(tab_id).style.display = 'block';
                                 }
                             })
 
@@ -220,7 +227,7 @@ class WPUF_Admin_Settings {
 
                     function wpuf_search_reset() {
                         content.forEach(function (row, index) {
-                            var content_id = row.getAttribute('id');
+                            var content_id = row.closest('div').getAttribute('id');
                             var tab_id     = content_id + '-tab';
                             document.getElementById(content_id).style.display = '';
                             document.getElementById(tab_id).style.display = '';
@@ -232,6 +239,7 @@ class WPUF_Admin_Settings {
 
                     }
                 });
+                })();
             </script>
         </div>
         <?php

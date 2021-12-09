5e675d64ceb1c2825048027056626472fd4f8993
sapayth <safayet.qubee@gmail.com>
2021-12-09 09:44:48 +0600

    fix: csfix

--- wpuf-functions.php
+++ wpuf-functions.php
@@ -519,11 +519,11 @@ function wpuf_get_image_sizes_array( $size = '' ) {
     $sizes = [];
 
     // Create the full array with sizes and crop info
-    foreach( $intermediate_image_sizes as $_size ) {
-        if ( in_array( $_size, [ 'thumbnail', 'medium', 'large', 'medium_large' ] ) ) {
+    foreach ( $intermediate_image_sizes as $_size ) {
+        if ( in_array( $_size, [ 'thumbnail', 'medium', 'large', 'medium_large' ], true ) ) {
             $sizes[ $_size ]['width']  = get_option( $_size . '_size_w' );
             $sizes[ $_size ]['height'] = get_option( $_size . '_size_h' );
-            $sizes[ $_size ]['crop']   = ( bool ) get_option( $_size . '_crop' );
+            $sizes[ $_size ]['crop']   = (bool) get_option( $_size . '_crop' );
         } elseif ( isset( $additional_image_sizes[ $_size ] ) ) {
             $sizes[ $_size ] = [
                 'width'  => $additional_image_sizes[ $_size ]['width'],
@@ -535,7 +535,7 @@ function wpuf_get_image_sizes_array( $size = '' ) {
 
     // Get only 1 size if found
     if ( $size ) {
-        if( isset( $sizes[ $size ] ) ) {
+        if ( isset( $sizes[ $size ] ) ) {
             return $sizes[ $size ];
         } else {
             return false;

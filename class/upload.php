2b1a17d0eaf3bb686ccd788d15af9505162345d3
Sapayth Hossain <safayet.qubee@gmail.com>
2022-11-24 12:49:03 +0600

    random time hashing removed

--- class/upload.php
+++ class/upload.php
@@ -85,11 +85,9 @@ class WPUF_Upload {
 
         $file_name      = pathinfo( $wpuf_file['name'], PATHINFO_FILENAME );
         $file_extension = pathinfo( $wpuf_file['name'], PATHINFO_EXTENSION );
-        $hash           = wp_hash( time() );
-        $hash           = substr( $hash, 0, 8 );
 
         $upload = [
-            'name'     => $file_name . '-' . $hash . '.' . $file_extension,
+            'name'     => $file_name . '.' . $file_extension,
             'type'     => $wpuf_file['type'],
             'tmp_name' => $wpuf_file['tmp_name'],
             'error'    => $wpuf_file['error'],

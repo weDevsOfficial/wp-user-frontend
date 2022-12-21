429b49d74f78539627e4ff2ab9ed0235f513b994
Nurul Amin Ratul <nurul.ratul@Nuruls-MacBook-Air.local>
2022-12-21 12:29:28 +0600

    PF-Blank + FEcheck > 00001

--- tests/tests/login.spec.ts
+++ tests/tests/login.spec.ts
@@ -47,16 +47,16 @@ test.describe('TEST :-->', () => {
 // //TODO: UNDO
 //Check_FRONT_END > BLANK_POST_Form-3.0
 
-    // test('0003: Check Front-End for BLANK Template', async ({ page }) => {
-    //     //const homePage = new HomePage(page);
-    //     const loginPage = new LoginPage(page);
-    //     const postFormsFrontEnd = new PostFormsFrontEnd(page);
+    test('0003: Check Front-End for BLANK Template', async ({ page }) => {
+        //const homePage = new HomePage(page);
+        const loginPage = new LoginPage(page);
+        const postFormsFrontEnd = new PostFormsFrontEnd(page);
         
-    // // await homePage.open();
-    //     await loginPage.login(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD);
-    //     await postFormsFrontEnd.checkNewPostBlankFormFrontEnd(process.env.NEW_POST_BLANKFORM_FRONTEND_POST_TITLE); 
-    //     //TODO: Add assertion
-    // });
+    // await homePage.open();
+        await loginPage.login(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD);
+        await postFormsFrontEnd.checkNewPostBlankFormFrontEnd(process.env.NEW_POST_BLANKFORM_FRONTEND_POST_TITLE); 
+        //TODO: Add assertion
+    });
 
 
 

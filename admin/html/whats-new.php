6b2991f41fa090afaf298a215553305c36079cf4
Sapayth Hossain <sapayth@users.noreply.github.com>
2023-08-31 12:50:51 +0600

    chor: bump version to 3.6.8

--- admin/html/whats-new.php
+++ admin/html/whats-new.php
@@ -1,8 +1,26 @@
 <?php
 $changelog = [
     [
+        'version'  => 'Version 3.6.8',
+        'released' => '2023-08-31',
+        'changes'  => [
+            [
+                'title' => __( 'Delete upload file even after clicking cancel button', 'wp-user-frontend' ),
+                'type'  => 'Fix',
+            ],
+            [
+                'title' => __( 'Taxonomy data not showing for checkbox', 'wp-user-frontend' ),
+                'type'  => 'Fix',
+            ],
+            [
+                'title' => __( 'Some validation for AJAX actions', 'wp-user-frontend' ),
+                'type'  => 'Fix',
+            ],
+        ],
+    ],
+    [
         'version'  => 'Version 3.6.7',
-        'released' => '2023-07-25',
+        'released' => '2023-08-09',
         'changes'  => [
             [
                 'title' => __( 'conflicts with Advanced Custom Fields Pro Plugin', 'wp-user-frontend' ),

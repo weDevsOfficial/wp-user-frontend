93cbcd22ba38d8ea733e2c40bff554b9649e2930
Rubaiyat-E-Mohammad <trubaiyatemohammad@gmail.com>
2025-03-13 15:16:44 +0600

    ok

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -57,7 +57,7 @@ const config: PlaywrightTestConfig = {
         // Artifacts
         screenshot: "only-on-failure",
 
-        video: "on",
+        video: "off",
 
         //SlowMo
         launchOptions: {

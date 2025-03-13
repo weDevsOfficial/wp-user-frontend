e0ddecd45a5694cefe932e1077e0cd82ad8f01c7
Rubaiyat-E-Mohammad <trubaiyatemohammad@gmail.com>
2025-03-13 12:35:51 +0600

    Introduced HTML reporting on GH pages

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -31,11 +31,13 @@ const config: PlaywrightTestConfig = {
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI
-        ? [["html"], ["junit", { outputFile: "playwright-report/results.xml" }]]
+        ? [
+            ["list", { printSteps: true }],
+            ["html", { outputFolder: "./playwright-report", open: "never" }]
+        ]
         : [
-              ["html", { open: "never" }],
-              ["list", { printSteps: true }],
-          ],
+            ["html", { outputFolder: "./playwright-report", open: "never" }],
+        ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -72,58 +74,7 @@ const config: PlaywrightTestConfig = {
                 storageState: "state.json",
             },
         },
-
-        // {
-        //   name: 'firefox',
-        //   use: {
-        //     ...devices['Desktop Firefox'],
-        //   },
-        // },
-
-        // {
-        //   name: 'webkit',
-        //   use: {
-        //     ...devices['Desktop Safari'],
-        //   },
-        //},
-
-        /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: {
-        //     ...devices['Pixel 5'],
-        //   },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: {
-        //     ...devices['iPhone 12'],
-        //   },
-        // },
-
-        /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: {
-        //     channel: 'msedge',
-        //   },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: {
-        //     channel: 'chrome',
-        //   },
-        // },
     ],
-
-    /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-    // outputDir: 'test-results/',
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   port: 3000,
-    // },
 };
 
 export default config;

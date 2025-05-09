296c105fba4c7978deb259c1161bbc418cf9ec19
Rubaiyat E Mohammad <trubaiyatemohammad@gmail.com>
2025-05-09 12:52:45 +0600

    Update wpuf.php

--- wpuf.php
+++ wpuf.php
@@ -23,7 +23,7 @@ if ( file_exists( $autoload ) ) {
     require_once $autoload;
 }
 
-define( 'WPUF_VERSION', '4.1.3' );
+define( 'WPUF_VERSION', '4.1.4' );
 define( 'WPUF_FILE', __FILE__ );
 define( 'WPUF_ROOT', __DIR__ );
 define( 'WPUF_ROOT_URI', plugins_url( '', __FILE__ ) );

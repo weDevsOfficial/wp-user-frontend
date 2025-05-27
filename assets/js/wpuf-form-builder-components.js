ac6cc70fef8cc68fbf54f009db4fd15aa3112fde
Sapayth Hossain <sapayth@users.noreply.github.com>
2025-05-27 16:02:57 +0600

    fix: custom taxonomies are not selectable

--- assets/js/wpuf-form-builder-components.js
+++ assets/js/wpuf-form-builder-components.js
@@ -441,6 +441,42 @@ Vue.component('field-multiselect', {
                     value: value
                 });
             }
+        },
+
+        // Dynamic options for taxonomy fields
+        dynamic_options: function () {
+            // Check if this is a Selection Terms field for a taxonomy
+            if (this.option_field.name === 'exclude' && 
+                this.editing_form_field && 
+                this.editing_form_field.input_type === 'taxonomy' &&
+                this.editing_form_field.name) {
+                
+                var taxonomy_name = this.editing_form_field.name;
+                
+                // Look for terms in the wp_post_types data
+                if (wpuf_form_builder && wpuf_form_builder.wp_post_types) {
+                    for (var post_type in wpuf_form_builder.wp_post_types) {
+                        var taxonomies = wpuf_form_builder.wp_post_types[post_type];
+                        
+                        if (taxonomies && taxonomies.hasOwnProperty(taxonomy_name)) {
+                            var tax_field = taxonomies[taxonomy_name];
+                            
+                            if (tax_field && tax_field.terms && tax_field.terms.length > 0) {
+                                var options = {};
+                                tax_field.terms.forEach(function(term) {
+                                    if (term && term.term_id && term.name) {
+                                        options[term.term_id] = term.name;
+                                    }
+                                });
+                                return options;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            // Return original options if not a taxonomy field or no dynamic options found
+            return this.option_field.options || {};
         }
     },
 
@@ -448,14 +484,27 @@ Vue.component('field-multiselect', {
         this.bind_selectize();
     },
 
+    watch: {
+        dynamic_options: function () {
+            // Refresh selectize when options change
+            this.$nextTick(function () {
+                this.bind_selectize();
+            });
+        }
+    },
+
     methods: {
         bind_selectize: function () {
             var self = this;
 
-            $(this.$el).find('.term-list-selector').selectize({}).on('change', function () {
-                var data = $(this).val();
+            // Destroy existing selectize if it exists
+            var $select = $(this.$el).find('.term-list-selector');
+            if ($select[0] && $select[0].selectize) {
+                $select[0].selectize.destroy();
+            }
 
-                self.value = data;
+            $select.selectize({}).on('change', function () {
+                self.value = $( this ).val();
             });
         },
     },

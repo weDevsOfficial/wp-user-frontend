cbfbf9fbcddcd78f00699069617c3a6bd42dadc2
arifulhoque7 <hoquea57@gmail.com>
2025-06-02 09:59:46 +0600

    fix: make common social field

--- assets/js-templates/form-components.php
+++ assets/js-templates/form-components.php
@@ -117,10 +117,12 @@
                         class="wpuf-block wpuf-text-sm wpuf-font-medium wpuf-leading-6 wpuf-text-gray-900">
                         {{ field.label }} <span v-if="field.required && 'yes' === field.required"
                                                 class="required">*</span>
-                        <template v-if="field.template === 'twitter_url' && field.show_icon === 'yes'">
-                            <svg class="wpuf-twitter-svg wpuf-inline wpuf-ml-2" width="20" height="20" viewBox="0 0 24 24" fill="#1da1f2" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
-                            </svg>
+                        <template v-if="field.show_icon === 'yes' && field.template && field.template.endsWith('_url')">
+                            <template v-if="field.template === 'twitter_url'">
+                                <svg class="wpuf-social-svg wpuf-twitter-svg wpuf-inline wpuf-ml-2" width="20" height="20" viewBox="0 0 24 24" fill="#1da1f2" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
+                                </svg>
+                            </template>
                         </template>
                     </label>
                 </div>
@@ -927,7 +929,7 @@
                                 <span :class="action_button_classes">
                                 <i
                                     class="fa fa-clone control-button-disabled wpuf--ml-1"></i>
-                            </span>
+                                </span>
                             </template>
                             <span :class="action_button_classes" @click="delete_column_field(innerIndex, column)">
                                 <svg class="wpuf-mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">

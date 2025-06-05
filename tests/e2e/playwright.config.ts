05221d5523695d1d22c22add833e5e1d23c30ef1
Rubaiyat-E-Mohammad <trubaiyatemohammad@gmail.com>
2025-06-05 14:39:01 +0600

    Go for it

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     testDir: './tests',
     /* Maximum time one test can run for. */
-    timeout: 10000, //10 sec
+    timeout: 5000, //10 sec
     expect: {
         /**
          * Maximum time expect() should wait for the condition to be met.

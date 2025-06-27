1be0bdda4247bacdf6fd7c4aac9d61b04eede3f8
Rubaiyat-E-Mohammad <trubaiyatemohammad@gmail.com>
2025-06-27 19:04:34 +0600

    Stablished updated post notification

--- tests/e2e/tests/postFormSettingsTest.spec.ts
+++ tests/e2e/tests/postFormSettingsTest.spec.ts
@@ -632,7 +632,7 @@ export default function postFormSettingsTest() {
             await postFormSettings.setMultipleUpdatedNotificationEmails(formName, multipleEmails);
         });
 
-        test.skip('PFS0078 : Admin is submitting post and validating Updated post notification from FE', { tag: ['@Pro'] }, async () => {
+        test('PFS0078 : Admin is submitting post and validating Updated post notification from FE', { tag: ['@Pro'] }, async () => {
             const previousPostTitle = postTitle;
             postTitle = faker.word.words(3);
             postContent = faker.lorem.paragraph();

6e3bcddc838c2b39ce2e79965d55fe530a8aeac2
arifulhoque7 <hoquea57@gmail.com>
2025-07-28 11:42:42 +0600

    fix: selected fix

--- assets/js/wpuf-form-builder-components.js
+++ assets/js/wpuf-form-builder-components.js
@@ -828,6 +828,59 @@ Vue.component('field-select', {
         };
     },
 
+    mounted: function() {
+        // Initialize selectedOption when component mounts
+        this.initializeSelectedOption();
+    },
+
+    watch: {
+        value: {
+            handler: function(newVal) {
+                // Update selectedOption when value changes
+                
+                this.initializeSelectedOption();
+            },
+            immediate: true
+        },
+        'editing_form_field': {
+            handler: function(newVal, oldVal) {
+                // When the entire editing_form_field object changes (like on data load)
+                this.initializeSelectedOption();
+            },
+            deep: true
+        },
+        'option_field.options': {
+            handler: function(newVal) {
+                // When options change, reinitialize
+                this.initializeSelectedOption();
+            },
+            deep: true
+        }
+    },
+
+    methods: {
+        initializeSelectedOption: function() {
+            var self = this;
+            this.$nextTick(function() {
+                // Get the current value
+                var currentValue = self.editing_form_field[self.option_field.name];
+                
+                if (currentValue && self.option_field.options && self.option_field.options[currentValue]) {
+                    self.selectedOption = self.option_field.options[currentValue];
+                } else if (!currentValue && self.option_field.default && self.option_field.options && self.option_field.options[self.option_field.default]) {
+                    // If no value but there's a default, show the default
+                    self.selectedOption = self.option_field.options[self.option_field.default];
+                    // Also set the value to default if there's no current value
+                    if (!currentValue) {
+                        self.value = self.option_field.default;
+                    }
+                } else {
+                    self.selectedOption = 'Select an option';
+                }
+            });
+        }
+    },
+
     computed: {
         value: {
             get: function () {

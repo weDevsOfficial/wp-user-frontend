adbfebbb378403d4d1a2bdab49de02f611527991
arifulhoque7 <hoquea57@gmail.com>
2025-08-03 21:44:54 +0600

    fix: removed usused cat from template

--- includes/Admin/template-parts/modal-v4.2.php
+++ includes/Admin/template-parts/modal-v4.2.php
@@ -25,14 +25,14 @@ if ( strpos( strtolower( $form_type ), 'registration' ) !== false || strpos( str
             'label'    => __( 'Membership', 'wp-user-frontend' ),
             'keywords' => [ 'membership' ],
         ],
-        'community'    => [
-            'label'    => __( 'Community', 'wp-user-frontend' ),
-            'keywords' => [],
-        ],
-        'associations' => [
-            'label'    => __( 'Associations', 'wp-user-frontend' ),
-            'keywords' => [],
-        ],
+        // 'community'    => [
+        //     'label'    => __( 'Community', 'wp-user-frontend' ),
+        //     'keywords' => [],
+        // ],
+        // 'associations' => [
+        //     'label'    => __( 'Associations', 'wp-user-frontend' ),
+        //     'keywords' => [],
+        // ],
     ];
 } else {
     // Post form categories
